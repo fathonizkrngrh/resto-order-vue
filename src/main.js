9530242bce0b88257c5fcfd34c8d3208816f5093
Fathoni Zikri Nugroho <fathonizikri11@gmail.com>
2022-12-29 07:59:42 +0700

    first init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap' from 'bootstrap/dist/css/bootstrap.min.css'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import './app.scss'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
   render: h => h(App),
